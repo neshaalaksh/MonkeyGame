97a0ed26db80c187021d3e953a97a3f6652980d8
neshaalakh <scientistneshaa@gmail.com>
2020-12-02 18:03:47 +0530

    added bg and finished the game

--- sketch.js
+++ sketch.js
@@ -6,9 +6,9 @@ var score
 var ground;
 var banana;
 function preload(){
-
+  bg=loadImage('jungle.jpg');
   
-  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  monkey_running =loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
   
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png");
@@ -19,12 +19,13 @@ function preload(){
 
 function setup() {
   createCanvas(600,400);
-  ground=createSprite(300,400,600,30);
+  ground=createSprite(300,350,600,30);
+  ground.visible=false;
   //ground.velocityX=5;
   //ground.x = ground.width/2;
   monkey=createSprite(300,350,30,30);
   monkey.addAnimation('ma',monkey_running);
-  monkey.scale=0.13;
+  monkey.scale=0.08;
   bananaGroup=new Group();
   obstaclesGroup=new Group();
   score=0
@@ -32,18 +33,39 @@ function setup() {
 
 
 function draw() {
-  background('white')
+  background(bg);
+
+  // bgg=createSprite(300,200,600,400);
+  // bgg.addImage('ni',bg);
+  stroke('white')
   text("Score: "+ score, 500,50);
   if(keyDown("space")&& monkey.y >= 50 ) {
         monkey.velocityY = -12;
         
     }
-  score = score + Math.round(getFrameRate()/60);
   monkey.velocityY = monkey.velocityY + 0.8
   monkey.collide(ground);
   //if (ground.x < 0){
     //  ground.x = ground.width/2;
    // }
+  if (bananaGroup.isTouching(monkey)){
+    score+=2;
+    bananaGroup.destroyEach();
+  }
+  if (obstaclesGroup.isTouching(monkey)){
+    monkey.scale=0.2;
+  }
+  switch(score){
+    case 10: monkey.scale=0.12;
+      break;
+    case 20: monkey.scale=0.14;
+      break;
+    case 30: monkey.scale=0.16;
+      break;
+    case 40: monkey.scale=0.18;
+      break;
+    default: break;
+  }
   food()
   spawnObstacles()
   drawSprites()
@@ -62,18 +84,13 @@ function food(){
 
 function spawnObstacles() {
   if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,360,40,40);
+    var obstacle = createSprite(600,350,40,40);
     //obstacle.debug = true;
     obstacle.velocityX = -10;
-    obstacle.addImage(obstacleImage);
+    obstacle.addImage('oi',obstacleImage);
     //assign scale and lifetime to the obstacle           
     obstacle.scale = 0.13;
     //add each obstacle to the group
     obstaclesGroup.add(obstacle);
   }
 }
-
-
-
-
-
